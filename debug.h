1bc5529b3b489bb47a17d33fef585f429f7be089
Yuxiao Zhang <octopusnju@gmail.com>
2017-09-01 10:56:58 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,28 +1,39 @@
+/* User defined DEBUG:
+ *  print messages to screen
+ *
+ * Keys:
+ *  1. <__VA_ARGS__> is a macro that represents the whole part in <...>, <...> can only at the end like ( a, b, ... ) not ( a, ..., b )
+ *  2. __FILE__ is a real string, __func__ is a string pointer (const char *)
+ *
+ * Author: yxzhang
+ * Date: 2017-09-01
+ */
+
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
 #include <stdio.h>
 
-#define __INFO__
-#define __WARN__
-#define __ERROR__
+#define __INFO__ 1
+#define __WARN__ 1
+#define __ERROR__ 1
 
-#ifdef __INFO__
-#define INFO(format,...) printf("FILE: "__FILE__", LINE: %04d, INFO: "format"\n", __LINE__, ##__VA_ARGS__)
+#if __INFO__
+#define INFO( format, ... ) printf("FILE:" __FILE__ ", FUNC:%s, LINE:%04d, INFO:" format "\n", __func__, __LINE__, __VA_ARGS__)
 #else
-#define INFO(format,...)
+#define INFO( format, ... )
 #endif
 
-#ifdef __WARN__
-#define WARN(format,...) printf("FILE: "__FILE__", LINE: %04d, WARN: "format"\n", __LINE__, ##__VA_ARGS__)
+#if __WARN__
+#define WARN( format, ... ) printf("FILE:" __FILE__ ", FUNC:%s, LINE:%04d, WARN:" format "\n", __func__, __LINE__, __VA_ARGS__)
 #else
-#define WARN(format,...)
+#define WARN( format, ... )
 #endif
 
-#ifdef __ERROR__
-#define ERROR(format,...) printf("FILE: "__FILE__", LINE: %04d, ERROR: "format"\n", __LINE__, ##__VA_ARGS__)
+#if __ERROR__
+#define ERROR( format, ... ) printf("FILE:" __FILE__ ", FUNC:%s, LINE:%04d, ERROR:" format "\n", __func__, __LINE__, __VA_ARGS__)
 #else
-#define ERROR(format,...)
+#define ERROR( format, ... )
 #endif
 
 #endif /*__DEBUG_H__*/
